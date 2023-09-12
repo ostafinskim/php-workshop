a43c9249abf18dc04a1cd828ff3dd04c53c8643b
Miro Ostafiński <ostafinskim@gmail.com>
2023-09-12 11:58:16 +0100

    feat: validation intro

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -5,8 +5,18 @@
 
     $title = "Create a new note";
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $errors = [];
+
+        if (strlen($_POST['body']) === 0) {
+            $errors['body'] = 'The note is required.';
+        } 
+
+        if (strlen($_POST['body']) > 1000) {
+            $errors['body'] = 'The note must be less than 255 characters.';
+        }
+
+        if ( empty($errors) ) {
             $db->query('INSERT INTO notes (body, user_id) VALUES (:body, :user_id)', [
                 'body' => $_POST['body'],
                 'user_id' => 1
@@ -14,7 +24,6 @@
             header('Location: /notes');
             exit;
         }
-
     }
 
     require 'views/note-create.view.php';
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -4,12 +4,26 @@
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+
         <form class="mt-8" method="POST">
+
+            <?php if (isset($errors['body'])) : ?>
+                <p class="text-red-500 my-3"><?= $errors['body'] ?></p>
+            <?php endif; ?>
+
             <div class="mb-4">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="body">
                     Your note:
                 </label>
-                <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="body" id="body"></textarea>
+                <textarea 
+                    cols="30" 
+                    rows="15" 
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                    name="body"
+                    id="body"
+                    required>
+                <?php $_POST['body'] ?? '' ?>
+                </textarea>
             </div>
             <div class="flex items-center justify-between">
                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
@@ -17,6 +31,7 @@
                 </button>
             </div>
         </form>
+        
     </div>
 </main>
 
